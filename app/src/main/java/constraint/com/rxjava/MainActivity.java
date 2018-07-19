f09eabd783daacb41481a3674e473a79d5206a2a
milan.m <milan.m@innovify.in>
2018-07-19 13:33:12 +0530

    Basic Example

---
+++ app/src/main/java/constraint/com/rxjava/MainActivity.java
@@ -0,0 +1,52 @@
+package constraint.com.rxjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Observable<String> booksObservable = Observable.just("c", "c++", "java", "python");
+        Observer<String> booksObserver = getBooksObserver();
+        booksObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(booksObserver);
+    }
+
+    public Observer<String> getBooksObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "Error");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete");
+            }
+        };
+    }
+}
